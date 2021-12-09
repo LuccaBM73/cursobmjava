764e12ba797b48c6269e160294799d6f5e0498a3
unknown <luccab73@gmail.com>
2021-12-09 15:32:23 -0300

    criar versão

---
+++ src/main/java/com/luccabm/cursobmjava/CursobmjavaApplication.java
@@ -0,0 +1,13 @@
+package com.luccabm.cursobmjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursobmjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursobmjavaApplication.class, args);
+	}
+
+}
