07fea12acaf0825e48b97a0c52a78216666044f4
unknown <luccab73@gmail.com>
2021-12-09 16:45:10 -0300

    Postman testado JSON ok

--- src/main/java/com/luccabm/cursobmjava/resources/CategoriaResources.java
+++ src/main/java/com/luccabm/cursobmjava/resources/CategoriaResources.java
@@ -1,19 +1,28 @@
 package com.luccabm.cursobmjava.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.luccabm.cursobmjava.domain.Categoria;
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value="/categorias") //Endpoint sempre em minusculo
 public class CategoriaResources 
 {
 	@RequestMapping(method=RequestMethod.GET) 
-	public String listar ()
+	public List<Categoria> listar()
 	{
 			
 			
-			return "REST ESTÁ FUNCIONANDO !";
+			Categoria cat1 = new Categoria(1,"Informática");
+			Categoria cat2 = new Categoria(2,"Escritorio");
+			
+			List<Categoria> lista = new ArrayList<>();	
+			lista.add(cat1);
+			lista.add(cat2);
+			
+			return lista;
 			
 		}
 	
